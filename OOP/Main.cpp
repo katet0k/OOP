44408d6257161be6685b664a2a66ecae366e6919
Катя <@DESKTOP-VT3CEPG>
2023-03-11 18:45:14 +0200

    0

---
+++ OOP/Main.cpp
@@ -0,0 +1,14 @@
+#include"menu.h"
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+
+	Menu* menu = new Menu;
+	menu->Vmenu();
+	return 0;
+}
