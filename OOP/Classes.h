9720600883a5977bbbb805343f815be3d632bde6
Катя <@DESKTOP-VT3CEPG>
2023-03-16 20:27:16 +0200

    0

--- OOP/Classes.h
+++ OOP/Classes.h
@@ -1,1 +1,109 @@
 #include"Library.h"
+
+class Book
+{
+private:
+    string author;
+    string name;
+    string publisher;
+    int year;
+    int number;
+    int pages;
+public:
+    explicit Book(string author, string name, string publisher, int year, int number, int pages)
+    {
+        this->author = author;
+        this->name = name;
+        this->number = number;
+        this->publisher = publisher;
+        this->year = year;
+        this->pages = pages;
+    }
+
+    string getAuthor() const
+    {
+        return author;
+    }
+    string getName() const
+    {
+        return name;
+    }
+    string getPublisher() const
+    {
+        return publisher;
+    }
+    int getYear() const
+    {
+        return year;
+    }
+    int getNumber() const
+    {
+        return number;
+    }
+    int getPages() const
+    {
+        return pages;
+    } 
+    void d() const
+    {
+        cout << " Author: " << author << endl;
+        cout << " Name: " << name << endl;
+        cout << " Publisher: " << publisher << endl;
+        cout << " Year: " << year << endl;
+        cout << " Number: " << number << endl;
+        cout << " Pages: " << pages << endl;
+        cout << endl;
+    }
+
+    Book() {
+        this->author = "Null";
+        this->name = "Null";
+        this->publisher = "Null";
+        this->number = 0;
+        this->year = 0;
+        this->pages = 0;
+    }
+    
+};
+
+class Worker {
+private:
+    string Name;
+    string Position;
+    string Year;
+    string Salary;
+public:
+
+    void set_Name(string Name) {
+        this->Name = Name;
+    }
+    void set_Position(string Position) {
+        this->Position = Position;
+    }
+    void set_Year(string Year) {
+        this->Year = Year;
+    }
+    void set_Salary(string Salary) {
+        this->Salary = Salary;
+    }
+
+    string get_Name() {
+        return Name;
+    }
+    string get_Position() {
+        return Position;
+    }
+    string get_Year() {
+        return Year;
+    }
+    string get_Salary() {
+        return Salary;
+    }
+
+    Worker() {
+        set_Name("Null");
+        set_Position("Null");
+        set_Year("Null");
+        set_Salary("Null");
+    }
+};
--- OOP/Main.cpp
+++ OOP/Main.cpp
@@ -1,15 +1,77 @@
 #include"menu.h"
 #include"Classes.h"
 
-int main() {
+void Print_Worker(Worker * worker)
+{
+    cout << " PIB: " << worker->get_Name() << endl;
+    cout << " Position: " << worker->get_Position << endl;
+    cout << " Year: " << worker->get_Year << endl;
+    cout << " Salary: " << worker->Salary << endl;
+    cout << endl;
+}
 
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
 
-	Menu* menu = new Menu;
-	menu->Vmenu();
-	return 0;
+    Menu* menu = new Menu;
+    menu->Vmenu();
+
+    char switch_on;
+    switch_on = _getch();
+
+    do {
+        switch (switch_on)
+        {
+        case '1':
+        {
+            const int Books = 5;
+            Book books[Books] = {
+              Book("J.K. Rowling", "Harry Potter and the Philosopher's Stone", "Bloomsbury Publishing", 1997, 500, 223),
+              Book("J.K. Rowling", "Harry Potter and the Chamber of Secrets", "Bloomsbury Publishing", 1998, 600, 251),
+              Book("J.R.R. Tolkien", "The Hobbit", "George Allen & Unwin", 1937, 300, 310),
+              Book("J.R.R. Tolkien", "The Lord of the Rings", "George Allen & Unwin", 1954, 1500, 1000),
+              Book("George Orwell", "1984", "Secker and Warburg", 1949, 1000, 328),
+            };
+            string author_name = "J.K. Rowling";
+            cout << "Books by " << author_name << ":" << endl;
+            for (const auto& book : books) {
+                if (book.getAuthor() == author_name) {
+                    book.d();
+                    cout << endl;
+                }
+            }
+            string publisher_name = "George Allen & Unwin";
+            cout << "Books published by " << publisher_name << ":" << endl;
+            for (const auto& book : books) {
+                if (book.getPublisher() == publisher_name) {
+                    book.d();
+                    cout << endl;
+                }
+            }
+            int year_published = 1950;
+            cout << "Books published after " << year_published << ":" << endl;
+            for (const auto& book : books) {
+                if (book.getYear() > year_published) {
+                    book.d();
+                    cout << endl;
+                }
+            }
+
+        }break;
+        case '2': {
+            Worker* worker = new Worker;
+        }break;
+        default:
+            break;
+        }
+
+    } while (switch_on != '0');
+
+   
+    return 0;
 }
