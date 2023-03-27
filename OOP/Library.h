f2d3da75889780d92b50dd0866e9f5511d30c8d9
Катя <@DESKTOP-VT3CEPG>
2023-03-27 18:00:03 +0300

    0

--- OOP/Library.h
+++ OOP/Library.h
@@ -1,4 +1,4 @@
-#include<iostream>
-#include<Windows.h>
-
+#include <iostream>
+#include <string>
+#include <Windows.h>
 using namespace std;
--- OOP/application.cpp
+++ OOP/application.cpp
@@ -1,6 +1,13 @@
-#include "Library.h"
+#include "menu.h"
 
 int main() {
-	
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+
+	Menu* menu = new Menu;
+	menu->Vmenu();
 	return 0;
 }
