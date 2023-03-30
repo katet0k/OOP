64f11e59b2097c082c814e4ee0d80d9a7107b78e
Катя <@DESKTOP-VT3CEPG>
2023-03-30 20:22:35 +0300

    0

--- OOP/Classes.h
+++ OOP/Classes.h
@@ -1,7 +1,72 @@
 
 class Overcoat {
 private:
-
+    string type_;
+    double price_;
 public:
+    Overcoat(const string& type = "", double price = 0)
+        : type_(type), price_(price) {}
+
+    string getType() const { return type_; }
+    void setType(const string& type) { type_ = type; }
+
+    double getPrice() const { return price_; }
+    void setPrice(double price) { price_ = price; }
+
+    bool operator==(const Overcoat& other) const {
+        return type_ == other.type_;
+    }
+
+    Overcoat& operator=(const Overcoat& other) {
+        type_ = other.type_;
+        price_ = other.price_;
+        return *this;
+    }
+
+    bool operator>(const Overcoat& other) const {
+        if (type_ != other.type_) {
+            std::cerr << "Error: cannot compare prices of different types of overcoats.\n";
+            return false;
+        }
+        return price_ > other.price_;
+    }
 
 };
+
+class Flat {
+private:
+    double area_;
+    double price_;
+public:
+    Flat(double area = 0, double price = 0)
+        : area_(area), price_(price) {}
+
+    double getArea() const { return area_; }
+    void setArea(double area) { area_ = area; }
+
+    double getPrice() const { return price_; }
+    void setPrice(double price) { price_ = price; }
+
+    bool operator==(const Flat& other) const {
+        return area_ == other.area_;
+    }
+
+    Flat& operator=(const Flat& other) {
+        if (this != &other) {
+            area_ = other.area_;
+            price_ = other.price_;
+        }
+        return *this;
+    }
+
+    bool operator>(const Flat& other) const {
+        return price_ > other.price_;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Flat& flat) {
+        os << "Area: " << flat.area_ << " square meters, Price: " << flat.price_ << " dollars";
+        return os;
+    }
+
+};
+
--- OOP/Main.cpp
+++ OOP/Main.cpp
@@ -1,5 +1,6 @@
 #include"menu.h"
 #include"Classes.h"
+
 int main() {
 
 	SetConsoleCP(1251);
@@ -10,5 +11,65 @@ int main() {
 
 	Menu* menu = new Menu;
 	menu->Vmenu();
+    char switch_on;
+
+    do
+    {
+        switch_on = _getch();
+        switch (switch_on)
+        {
+        case '1':
+        {
+            Overcoat oc1("wool", 200.0);
+            Overcoat oc2("wool", 150.0);
+            Overcoat oc3("trench", 300.0);
+
+            std::cout << std::boolalpha;
+            std::cout << "oc1 == oc2: " << (oc1 == oc2) << '\n'; // true
+            std::cout << "oc1 == oc3: " << (oc1 == oc3) << '\n'; // false
+
+            oc2 = oc1;
+            std::cout << "oc1 == oc2: " << (oc1 == oc2) << '\n'; // true
+
+            std::cout << "oc1 > oc2: " << (oc1 > oc2) << '\n'; // false
+            std::cout << "oc2 > oc1: " << (oc2 > oc1) << '\n'; // true
+            std::cout << "oc1 > oc3: "; (oc1 > oc3);
+
+        } break;
+
+        case '2':
+        {
+            Flat flat1(50, 100000);
+            Flat flat2(80, 150000);
+
+            cout << "Flat 1: " << flat1 << endl;
+            cout << "Flat 2: " << flat2 << endl;
+
+            // Check for equality of areas
+            if (flat1 == flat2) {
+                std::cout << "The flats have the same area" << std::endl;
+            }
+            else {
+                std::cout << "The flats have different areas" << std::endl;
+            }
+
+            // Assign flat2 to flat1
+            flat1 = flat2;
+            std::cout << "Flat 1 after assignment: " << flat1 << std::endl;
+
+            // Compare flats by price
+            if (flat1 > flat2) {
+                std::cout << "Flat 1 is more expensive than flat 2" << std::endl;
+            }
+            else {
+                std::cout << "Flat 2 is more expensive than flat 1" << std::endl;
+            }
+
+        }break;
+        default:
+            break;
+        }
+    } while (switch_on != '0');
+   
 	return 0;
 }
