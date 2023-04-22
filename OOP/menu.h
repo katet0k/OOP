ee6d8f2923c6fcf506c590672c39ec195d9f607a
Катя <@DESKTOP-VT3CEPG>
2023-04-22 19:13:01 +0300

    0

--- OOP/menu.h
+++ OOP/menu.h
@@ -5,24 +5,24 @@ public:
 	void Vmenu() {
 		cout << " #===============================# " << endl;
 		cout << "  |                             |  " << endl;
-		cout << "  |      1 - class �����        |  " << endl;
+		cout << "  |      1 - class Точка        |  " << endl;
 		cout << "  |                             |  " << endl;
-		cout << "  |      2 - class ���         |  " << endl;
+		cout << "  |      2 - class Дріб         |  " << endl;
 		cout << "  |                             |  " << endl;
-		cout << "  |      3 - class �������      |  " << endl;
+		cout << "  |      3 - class Студент      |  " << endl;
 		cout << "  |                             |  " << endl;
-		cout << "  |     4 - ��������� �����     |  " << endl;
+		cout << "  |     4 - Телефонна книга     |  " << endl;
 		cout << "  |                             |  " << endl;
 		cout << " #===============================# " << endl;
 	}
 	void MenuPhone() {
 		cout << endl;
 		cout << "                                                  " << endl;
-		cout << "     ��� ������� ������ �������� ��������� -- 1   " << endl;
+		cout << "     Для додання нового абонента натисніть -- 1   " << endl;
 		cout << "                                                  " << endl;
-		cout << "     ��� ��������� �������� ��������� -- 2        " << endl;
+		cout << "     Для Видалення абонента натисніть -- 2        " << endl;
 		cout << "                                                  " << endl;
-		cout << "     ��� ������ �������� ��������� -- 3           " << endl;
+		cout << "     Для пошуку абонента натисніть -- 3           " << endl;
 		cout << "                                                  " << endl << endl;
 	}
 	void DeleteMenu() {
